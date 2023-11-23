d5b806f1457d291ca4177c4c754d4d23f6ce4467
07HaleemaZehera <hzehera07@gmail.com>
2023-11-23 22:27:37 +0530

    files uploaded

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold text-red-400 text-5xl">
+     Hello world..................
+    </div>
+  );
+}
+
+export default App;
